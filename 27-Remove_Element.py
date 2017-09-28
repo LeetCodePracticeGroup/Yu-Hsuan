54326616aa8fbb2eff210cadb4cbc66be5d3411f
blureze <blureze@gmail.com>
2017-09-28 23:52:39 +0800

    quicker solution by using two pointers

--- 27-Remove_Element.py
+++ 27-Remove_Element.py
@@ -10,16 +10,11 @@ class Solution(object):
         
         if length == 0:
             return 0
-        if val not in nums:
-            return length
         
-        # sort
-        for i in range(length - 1):
-            if nums[i] == val:
-                for j in range(i+1, length):
-                    if nums[j] != val:
-                        tmp = nums[i]
-                        nums[i] = nums[j]
-                        nums[j] = tmp
-                        
-        return nums.index(val)
+        i = 0
+        for j in range(length):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i = i + 1
+                
+        return i
